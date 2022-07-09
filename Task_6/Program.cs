e0d5e76be74ae9b78fa33f99e7e9cabd3aef94e5
ArhipovVS <Arhipof05@yandex.ru>
2022-07-09 12:03:44 +0700

    Task_6

---
+++ Task_6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int num= Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.WriteLine($"Ввеедное число {num} четное");
+}
+else 
+{
+    Console.WriteLine($"введенное число {num} нечетное");
+} 
