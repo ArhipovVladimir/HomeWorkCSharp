6c5026cd517495ee74733381183a4ebc7a9577e5
ArhipovVS <Arhipof05@yandex.ru>
2022-07-11 16:00:15 +0700

    task_10

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -1,4 +1,13 @@
-﻿Console.Clear();
-Console.WriteLine("Веедите трехзначное число");
-int num = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(num);
+﻿// Задача 10: Напишите программу, которая принимает
+// на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+  Console.Clear();
+  Console.WriteLine("Веедите трехзначное число");
+  int num = Convert.ToInt32(Console.ReadLine());
+  int resalt = num;
+  resalt = resalt / 10;
+  resalt = resalt % 10;
+  Console.WriteLine(resalt);
