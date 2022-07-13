67a4c700fe27ac14dec26bbc8cb735d4a2bdd24b
ArhipovVS <Arhipof05@yandex.ru>
2022-07-13 23:25:11 +0700

    задача про полиндрои

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -1,18 +1,35 @@
 ﻿// Задача 19
-// Напишите программу, которая принимает на вход 
+// Напишите программу, которая принимает на вход
 // пятизначное число и проверяет, является ли оно палиндромом.
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
 
-int Palindr(int num)
+string Palindr(int num)
 {
-    return;
+    int rightPart = num / 1000;
+    int LeftPart = num % 100;
+    int LeftPartDoz = LeftPart / 10;
+    int leftPartUni = num % 10;
+    int retutnLeft = leftPartUni * 10 + LeftPartDoz;
+    if (rightPart == retutnLeft)
+    {
+        return "да";
+    }
+    else
+    {
+        return "нет";
+    }
 }
 
-Console.Cleaar();
+Console.Clear();
 Console.WriteLine("Введите пятизначное число");
 int digit = Convert.ToInt32(Console.ReadLine());
-
- Palindr (digit);
-
+if (digit >= 100000 || digit <= 9999)
+{
+    Console.WriteLine("Число непятизначное");
+}
+else
+{
+    Console.WriteLine($"{digit} -> {Palindr(digit)}");
+}
