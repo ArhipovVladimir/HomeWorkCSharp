3f58cc2a58d98d19df55aaa9f1bab3b75469b03b
ArhipovVS <Arhipof05@yandex.ru>
2022-07-16 13:22:16 +0700

    задача ро 3Д

--- Task_21/Program.cs
+++ Task_21/Program.cs
@@ -7,5 +7,62 @@
 
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-Console.WriteLine("веедите коррдинаты");
+double Dist (double ax, double ay, double az, double bx, double by, double bz)
+{   
+    double q = Math.Pow((bx - ax), 2) + Math.Pow((by - ay), 2)+ Math.Pow((bz - az), 2);
+    double q1 = Math.Sqrt(q);
+    return Math.Round(q1, 2, MidpointRounding.ToZero);
+   
+}
+
+Console.WriteLine ("Веедите Коородинаты первой точки");
+Console.Write ("X:");
+double x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Y:");
+double y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Z:");
+double z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Веедите Коородинаты второй точки");
+Console.Write ("X:");
+double x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Y:");
+double y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Z:");
+double z2 = Convert.ToInt32(Console.ReadLine());
+
+double result = Dist (x1, y1, z1, x2, y2, z2);
+
+Console.WriteLine ($"Растояние между точками {result}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// double [] Inwerkoor (string oneK, string secondK)
+// {
+//     double [] oneMass = oneK.split.();
+//     double [] secMass = secondK.split.();
+    
+//     foreach (double [] one in oneMass)
+//     {
+        
+//     }
+// }
+// Console.WriteLine("веедите коррдинаты первой точки, (кородинаты необходисмо вводить через пробел)");
+// string oneKoord = Console.ReadLine();
+// Console.WriteLine("веедите коррдинаты втрой точки");
+// string secondKoord = Console.ReadLine();
+// // Console.WriteLine (oneKoord);
+// // Console.WriteLine (secondKoord);
+// Inwerkoor (oneKoord, secondKoord)
 
