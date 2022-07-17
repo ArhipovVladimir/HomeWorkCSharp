7166d742e2279437507fbb97d8d4797f69455743
ArhipovVS <Arhipof05@yandex.ru>
2022-07-17 20:15:01 +0700

    задача про возведение в степень

---
+++ Task_25/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа 
+// (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+int PowAB (int a, int b)
+{
+    int res = 1;
+    for (int count=1; count <=b; count++)
+    {
+        res=res*a;
+    }
+    
+    return res;
+}
+
+
+Console.WriteLine("Введите два числа (А и В) компьютер выдаст ответ А в степени В");
+Console.Write("А:");
+int digitA = Convert.ToInt32(Console.ReadLine());
+digitA=Math.Abs(digitA);
+
+Console.Write("B:");
+int digitB = Convert.ToInt32(Console.ReadLine());
+digitB=Math.Abs(digitB);
+
+double resalt = PowAB(digitA, digitB);
+Console.WriteLine($"{digitA} в степени {digitB} Равно {resalt}");
