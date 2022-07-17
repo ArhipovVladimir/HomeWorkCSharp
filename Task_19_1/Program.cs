7790857b79a94d91a843ac1c61f65272a532d032
ArhipovVS <Arhipof05@yandex.ru>
2022-07-17 15:26:31 +0700

    универсальный алгоритм под любое число

---
+++ Task_19_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход
+// пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+string Palindr(int num)
+{
+    int revers = 0;
+    int numOrig = num;
+    while (num > 0)
+    {
+        int dig = num % 10;
+        revers = revers * 10 + dig;
+        num=num/10;
+    }
+
+    if (revers==numOrig) return "Да";
+    else return "Нет";
+}
+
+Console.Clear();
+Console.WriteLine("Введите число чтобы узнвть палиндром ли это");
+int digit = Convert.ToInt32(Console.ReadLine());
+digit = Math.Abs(digit);
+Console.WriteLine($"{digit} -> {Palindr(digit)}");
