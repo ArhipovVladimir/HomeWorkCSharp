5a601b165751165c3e735ccde9349d7cec89f7ea
ArhipovVS <Arhipof05@yandex.ru>
2022-08-19 19:05:44 +0700

    Task58

--- Task_58/Program.cs
+++ Task_58/Program.cs
@@ -7,3 +7,81 @@
 // 18 20
 // 15 18
 
+int GetUserInput()
+{
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int [,] GetMatrix1 (int m, int n)
+{
+    int [,] array = new int [m,n];
+    
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array [i, j] = rnd.Next(1, 10);
+        }
+    } 
+    return array;
+}
+
+ 
+void PrintMatrix (int [,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write ("[");
+            if (j < array.GetLength(1) - 1) Console.Write ($"{array[i,j],  3},");
+            else Console.Write ($"{array[i,j],  3}]");
+        }
+        Console.WriteLine();
+    } 
+
+}
+
+int [,] GetMatrix2 (int m, int n)
+{
+    int [,] array = new int [m,n];
+    
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array [i, j] = rnd.Next(1, 10);
+        }
+    } 
+    return array;
+}
+
+int [,] MatrixPow (int [,] matrix1, int [,] matrix2)
+{
+    int [,] matrixPow = new int [matrix1.GetLength(0), matrix1.GetLength(1)];
+    for (int i = 0; i < matrix1.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix1.GetLength(1); j++)
+        {
+            matrixPow [i,j] =matrix1[i,j]*matrix2[i,j];
+        }
+    }
+    return matrixPow;
+}
+
+Console.WriteLine("Введите размер масиива MxN");
+Console.Write("M:");
+int m = GetUserInput();
+Console.Write("N:");
+int n = GetUserInput();
+int [,] matrix1 = GetMatrix1 (m, n);
+int [,] matrix2 = GetMatrix2 (m, n);
+PrintMatrix(matrix1);
+Console.WriteLine();
+PrintMatrix(matrix2);
+int [,] matrixPow = MatrixPow (matrix1, matrix2);
+Console.WriteLine();
+PrintMatrix(matrixPow);
