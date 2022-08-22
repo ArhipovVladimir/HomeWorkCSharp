e5d16134f3df013347e24ce6eb354f9448be032a
ArhipovVS <Arhipof05@yandex.ru>
2022-08-22 22:56:20 +0700

    Task68

--- Task_68/Program.cs
+++ Task_68/Program.cs
@@ -10,11 +10,11 @@ double GetUserInput()
 }
 
 
-double akerman (double m, double n)
+double Akerman (double m, double n)
 {
     if (m == 0) return n + 1;
-    else if ((m > 0) && (n == 0)) return akerman (m - 1, 1);
-    else if ((m > 0) && (n > 0)) return akerman (m - 1, akerman (m, n - 1));
+    else if ((m > 0) && (n == 0)) return Akerman (m - 1, 1);
+    else if ((m > 0) && (n > 0)) return Akerman (m - 1, Akerman (m, n - 1));
     else return n + 1;
 
 }
@@ -25,7 +25,7 @@ Console.Write("M: ");
 double m = GetUserInput();
 Console.Write("N: ");
 double n = GetUserInput();
-double result = akerman (m, n);
+double result = Akerman (m, n);
 Console.WriteLine($" результат функции аккермвна  при параметрах {m} и {n} равен {result}");
 
 
